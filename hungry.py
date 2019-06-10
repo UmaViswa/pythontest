5ef10c4d5af56daeab773b361f3198491192455e
keerthanaMohanarangan <scanfkeertha@gmail.com>
2019-06-10 14:12:29 +0300

    if block added

--- hungry.py
+++ hungry.py
@@ -1,5 +1,11 @@
 hungry = input("Are you hungry: ")
 if hungry == "yes":
-    print("eat your food")
+    cook = input("do you have any food left?")
+    if cook == "yes":
+        print("Eat it")
+    else:
+        print("Then cook and eat")
+
+    print("Eat your food")
 else:
-    print("its time to sleep")
+    print("Its time to sleep")
